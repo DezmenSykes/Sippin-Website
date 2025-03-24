b005b447da45d71b87d191e9a7b96885bdb5857f
DezmenSykes <oddly.observant@gmail.com>
2025-03-24 15:05:36 -0500

    updated styling for logo and added images

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import SippinLogo from './SippinLogo'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='flex justify-center py-4 -mb-28'>
+        <SippinLogo className='h-20 z-10 cursor-pointer text-sky-800'/>
+    </header>
   )
 }
 
