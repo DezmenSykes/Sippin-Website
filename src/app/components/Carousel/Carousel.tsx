c4416e9248cc916f94a698b1155f18e9fa1f3357
DezmenSykes <oddly.observant@gmail.com>
2025-03-27 10:41:15 -0500

    added hover to btn

--- src/app/components/Carousel/Carousel.tsx
+++ src/app/components/Carousel/Carousel.tsx
@@ -146,7 +146,7 @@ const ArrowBtn = ({ label, onClick, direction = "right" }: ArrowBtnProps) => {
   return (
     <button
       onClick={onClick}
-      className="z-30 size-12 rounded-full border-2 border-white bg-white/10 p-3 opacity-85 ring-white focus:outline-none focus-visible:opacity-100 focus-visible:ring-4 md:size-16 lg:size-20"
+      className="z-30 size-12 rounded-full border-2 border-white bg-white/10 p-3 opacity-85 ring-white hover:bg-white/50 focus:outline-none focus-visible:opacity-100 focus-visible:ring-4 md:size-16 lg:size-20"
     >
       <ArrowIcon className={clsx(direction === "right" && "-scale-x-100")} />
       <span className="sr-only">{label}</span>
