3a37b23b45436022b9c22acb59415d43ba94bbaa
DezmenSykes <oddly.observant@gmail.com>
2025-03-27 14:50:54 -0500

    footer stuff

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,10 +1,21 @@
+'use client'
 import React from "react";
 import SippinLogo from "./SippinLogo";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP)
+
 
 const Footer = () => {
+
+  useGSAP(()=>{
+
+  })
+
   return (
-    <footer className="bg-[#fee832] text-[#fe6334] p-10 flex items-center">
-      <div className="relative flex items-center">
+    <footer className="bg-[#fee832] text-[#fe6334] pt-[10rem] pb-[2rem] flex justify-center">
+      <div className="relative">
         <SippinLogo />
       </div>
     </footer>
